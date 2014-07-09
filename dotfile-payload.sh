03bcfe0cc13a668d32a9fc2d437bb46ee783ef4e
jgr68 <john.ryan@rutgers.edu>
2014-07-09 15:12:20 -0400

    Added command line argument parsing.

--- dotfile-payload.sh
+++ dotfile-payload.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
-# get git information
-repo_name="dotfiles"
-git_user="jgr68"
+# -r <repo> repo_name
+# -g <git_username> git_user
+#
+#
+#
+
+usage() {
+
+	echo "Usage: $0 [-r <dotfiles git repo>] [-g <git username>] 1>&2";
+	exit 1;
+}
+
+while getopts "r:g:" opt; do
+
+	case $opt in
+		r)
+			echo "-r was triggered"
+			repo_name=$OPTARG
+			;;
+		g)
+			echo "-g was triggered"
+			git_user=$OPTARG
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+
+if [[ -z $repo_name ]] || [[ -z $git_user ]]; then
+	
+	usage;
+	exit 1;
+fi
+
+echo "You selected repo_name: $repo_name"
+echo "You selected git_user: $git_user"
+
 repo_url="git@github.com:$git_user/$repo_name.git"
 
 # change directory to home
